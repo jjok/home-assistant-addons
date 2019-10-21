143032b563b20394f5275f471b906d461adf71b4
jonathan <jonathan@jjok.co.uk>
2019-10-22 00:42:22 +0100

    Pass env to rebuild script

--- todo_two/run.sh
+++ todo_two/run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 cat /data/options.json
-php /app/todo-two-web-master/bin/rebuild.php
+APP_ENV=hassio php /app/todo-two-web-master/bin/rebuild.php
 APP_ENV=hassio php -d variables_order=EGPCS -S 0.0.0.0:8000 -t /app/todo-two-web-master/public
