799fbecdd73586e140767d3828b1ee7fdcba26cf
jonathan <jonathan@jjok.co.uk>
2021-05-21 02:21:26 +0100

    Use v1.0.0 of Todo Two Web

--- todo_two/run.sh
+++ todo_two/run.sh
@@ -2,5 +2,5 @@
 
 export APP_ENV=hassio
 
-php /app/todo-two-web-master/bin/rebuild.php
-php -d variables_order=EGPCS -S 0.0.0.0:8000 -t /app/todo-two-web-master/public
+php /app/bin/rebuild.php
+php -d variables_order=EGPCS -S 0.0.0.0:8000 -t /app/public
